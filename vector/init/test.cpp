59fe399ab7f9475ed52ebf2e29d59b6fef51afba
LHesperus <2639350497@qq.com>
2019-04-26 17:37:15 +0800

    Create test.cpp
    vector test

---
+++ vector/init/test.cpp
@@ -0,0 +1,20 @@
+﻿// test.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+	vector<int> ivec;
+	cout << ivec.empty() << endl;
+	for (int i = 0; i < 10; i++)
+	{
+		ivec.push_back(i);
+	}
+	cout << ivec.empty() << endl;
+	cout << ivec.size() << endl;
+	
+
+
+}
